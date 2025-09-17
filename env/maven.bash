423fc2f8a04661b1cfe4fff6e7b77f15d87f98a1
awei <455400490@qq.com>
2025-09-18 01:55:41 +0800

    refactor(msc-user): 初始化user模块

--- env/maven.bash
+++ env/maven.bash
@@ -1,2 +1,2 @@
-# 使用当前目录下的settings.xml进行打包
-mvn clean install -s ./env/settings.xml
+# 先清理项目，再清理本地缓存依赖并重新解析，最后安装，同时强制更新快照依赖
+mvn clean dependency:purge-local-repository install -U -s ./env/settings.xml -Dmaven.test.skip=true
